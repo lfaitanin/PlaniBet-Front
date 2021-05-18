46e8bc038c0c5012adf128970cb5455acf2c19da
Luiz Faitanin <lfaitanin@outlook.com>
2021-05-17 23:38:58 -0300

    subindo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home'
+import Login from './Components/Login'
+import SignUp from './Components/SignUp'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component= {Home} exact/>
+      <Route path="/Login" component= {Login}/>
+      <Route path="/SignUp" component= {SignUp}/> 
+      <Route component= {() => {<div>Pagina não encontrada!</div>}}/> 
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
